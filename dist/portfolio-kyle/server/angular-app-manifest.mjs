e1d9bc9d075d1c5feea6120ec60007c3ccc0db1b
Rostyslav Kovach <Rostyslav.Kovach@vanarts.com>
2025-06-28 15:43:45 -0700

    removed scroll icon

--- dist/portfolio-kyle/server/angular-app-manifest.mjs
+++ dist/portfolio-kyle/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 16169, hash: '488da6f2c70af549837cf2e1b5a10769bfc4ffbcce33bea63828079c5407d66e', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 16683, hash: '2bc1955cd50d85f07a9f0aed47b528ba1fff2689c26b53b1ec7240326d6c55dc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 16149, hash: 'f8521f3bb864287a91ffff40050076ef746c11be5c4c2f983f3bb1faf245c764', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 16663, hash: '62fe418a67b7b5cc08386d1858183abb7cead4863fcfc3cb70c12e669dae7586', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
--- dist/portfolio-kyle/server/assets-chunks/index_server_html.mjs
+++ dist/portfolio-kyle/server/assets-chunks/index_server_html.mjs
@@ -6,7 +6,7 @@ export default `<!doctype html>
   <base href="/">
   
   <meta name="theme-color" content="#010101"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Portfolio of Kyle Wrigley: 3D Environment and Prop Artist. Specializing in game-ready assets, immersive environments, and detailed props for games and virtual experiences – both stylized and realistic.">
 
   <link rel="icon" type="image/x-icon" href="favicon.png">
@@ -27,6 +27,6 @@ export default `<!doctype html>
 <body><script type="text/javascript" id="ng-event-dispatch-contract">(()=>{function p(t,n,r,o,e,i,f,m){return{eventType:t,event:n,targetElement:r,eic:o,timeStamp:e,eia:i,eirp:f,eiack:m}}function u(t){let n=[],r=e=>{n.push(e)};return{c:t,q:n,et:[],etc:[],d:r,h:e=>{r(p(e.type,e,e.target,t,Date.now()))}}}function s(t,n,r){for(let o=0;o<n.length;o++){let e=n[o];(r?t.etc:t.et).push(e),t.c.addEventListener(e,t.h,r)}}function c(t,n,r,o,e=window){let i=u(t);e._ejsas||(e._ejsas={}),e._ejsas[n]=i,s(i,r),s(i,o,!0)}window.__jsaction_bootstrap=c;})();
 </script>
   <app-root></app-root>
-<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-TXQJA2L3.js" type="module"></script></body>
+<script src="polyfills-FFHMD2TL.js" type="module"></script><script src="main-2VGZNH42.js" type="module"></script></body>
 </html>
 `;
