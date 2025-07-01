08d18b5f0b13a56af70d82a40d76c16e69284fe1
Rostyslav Kovach <Rostyslav.Kovach@vanarts.com>
2025-07-01 09:49:40 -0700

    changed cta order

--- dist/portfolio-kyle/server/angular-app-manifest.mjs
+++ dist/portfolio-kyle/server/angular-app-manifest.mjs
@@ -7,8 +7,8 @@ export default {
   routes: undefined,
   entryPointToBrowserMapping: {},
   assets: {
-    'index.csr.html': {size: 16169, hash: '969e9cc201119653d1ddc4d491d5fdc82d76fb5b1be6c88d8e7af06bef2db78b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 16683, hash: '6a85dbaebe96484e44a9657ad6c63bcfc309f87ea4333d2861f8f9b9ff885f40', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 16169, hash: '533b8c7d639d2e82c1194e04e2eee5eb5ef781f27af6fd7d6d66234d6cdd1b74', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 16683, hash: 'bd131b863e2eb2546b6f8c59e369b77323cff1e182bd73f47fa1fb820f53adda', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
